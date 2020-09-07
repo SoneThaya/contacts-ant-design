fcc45a7f3010470a90810823f17b42fa86645abd
Sone <sthaya@gmail.com>
2020-09-06 20:12:07 -0700

    adjust side bar menu and header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Button, Layout, Table, Menu, Breadcrumb } from 'antd';
 import { PlusSquareFilled } from '@ant-design/icons';
 import AddDrawer from './AddDrawer';
@@ -21,6 +21,10 @@ function App() {
   const [values, setValues] = useState([]);
   const [errorInfo, setErrorInfo] = useState({});
   const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (isCollapsed) => {
+    setCollapsed(isCollapsed );
+  };
 
   const handleAddFormOnFinish = (data) => {
     setValues([...values, {
@@ -55,59 +59,56 @@ function App() {
   return (
     <>
     <Layout style={{ minHeight: '100vh' }}>
-    <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+    <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
       <div className="logo" />
       <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Option 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<DesktopOutlined />}>
-          Option 2
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-          <Menu.Item key="3">Tom</Menu.Item>
-          <Menu.Item key="4">Bill</Menu.Item>
-          <Menu.Item key="5">Alex</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-          <Menu.Item key="6">Team 1</Menu.Item>
-          <Menu.Item key="8">Team 2</Menu.Item>
-        </SubMenu>
-        <Menu.Item key="9" icon={<FileOutlined />} />
+        
+       
       </Menu>
     </Sider>
     <Layout className="site-layout">
-      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Header style={{ padding: 0, background: '#ffffff' }} />
       <Content style={{ margin: '0 16px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>User</Breadcrumb.Item>
-          <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
         
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          <Fragment>
+              <div style={{
+                  display: 'flex',
+                  justifyContent: "space-between",
+                  marginBottom: 20
+                }}>
+              <div></div>
+              <div>
+                  <Button
+                  type="primary"
+                  icon={<PlusSquareFilled />}
+                  data-testid="add-contact-button"
+                  onClick={() => setShowDrawer(true)}
+                >
+                  Add
+                </Button>
+              </div>
+             
+            </div>
+            
+          <Layout.Content>
+            <Table dataSource={values} columns={columns} />
+          </Layout.Content>
+          
+          <AddDrawer
+            show={showDrawer}
+            handleOnClose={() => setShowDrawer(false)}
+            handleOnFinish={handleAddFormOnFinish}
+            handleOnFinishFailed={handleAddFormOnFinishFailed}
+          />
+        </Fragment>
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
     </Layout>
-  </Layout>
-      <Button
-        type="primary"
-        icon={<PlusSquareFilled />}
-        data-testid="add-contact-button"
-        onClick={() => setShowDrawer(true)}
-      >
-        Add
-      </Button>
-      <Layout.Content>
-        <Table dataSource={values} columns={columns} />
-      </Layout.Content>
+      </Layout>
       
-      <AddDrawer
-        show={showDrawer}
-        handleOnClose={() => setShowDrawer(false)}
-        handleOnFinish={handleAddFormOnFinish}
-        handleOnFinishFailed={handleAddFormOnFinishFailed}
-      />
+      
     </>
   );
 }
